c689816293d94ea76ab61952944446a5a186b466
Lucas <lucas_99silva@hotmail.com>
2021-05-07 16:09:18 -0300

    Challenge finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "../components/MovieCard";
+import { Header } from "../components/Header";
+
+interface ContentProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+  selectedGenre: {
+    title: string;
+  };
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <Header title={props.selectedGenre.title} />
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
